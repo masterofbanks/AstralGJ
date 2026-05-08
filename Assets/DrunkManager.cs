7fe12e47397b934fd781848c82d31e9d5e2fc4a2
Tarik Campbell <129552741+masterofbanks@users.noreply.github.com>
2026-05-08 17:38:52 -0400

    text mesh pro and some drunk shader code

---
+++ Assets/DrunkManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DrunkManager : MonoBehaviour
+{
+    private static DrunkManager _instance;
+    public static DrunkManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                GameObject drunkManagerObject = Instantiate(Resources.Load<GameObject>("DrunkManager"));
+                _instance = drunkManagerObject.GetComponent<DrunkManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+
+    [Header("Shader Values")]
+    [SerializeField] private Material DrunkShaderMaterial;
+
+
+    public void SetDrunkness(float drunknessLevel)
+    {
+        if(drunknessLevel < 0 || drunknessLevel > 1.0f)
+        {
+            Debug.Log($"Improper drunkness level set: {drunknessLevel}");
+            return;
+        }
+        DrunkShaderMaterial.SetFloat("Blend", drunknessLevel);
+    }
+}
