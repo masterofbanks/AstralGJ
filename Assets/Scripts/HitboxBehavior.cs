784364a4d862cb29a6cb44448a007e6ce83a36f7
Tarik Campbell <129552741+masterofbanks@users.noreply.github.com>
2026-05-10 15:31:44 -0400

    wine collectible that makes you drunker

--- Assets/Scripts/HitboxBehavior.cs
+++ Assets/Scripts/HitboxBehavior.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class HitboxBehavior : MonoBehaviour
 {
+    public GameObject DrinkingSFX;
     public void UpdatePosition(Vector3 position)
     {
         transform.position = position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wine"))
+        {
+            GameManager.Instance.MakeDrunker(1.0f);
+            Instantiate(DrinkingSFX, transform.position, Quaternion.identity);  
+            Destroy(collision.gameObject);
+        }
     }
 }
